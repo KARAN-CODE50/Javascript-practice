33a884c110875520699087d45a1dda770716dd91
Karan Sarawagi <75156504+KARAN-CODE50@users.noreply.github.com>
2023-10-17 17:37:57 +0000

    functions

---
+++ Basics/03_basics/functions.js
@@ -0,0 +1,17 @@
+function sayName(){
+    console.log("K")
+    console.log("a")
+    console.log("r")
+    console.log("a")
+    console.log("n")
+}
+
+// sayName    --> this is reference 
+// sayName()  --> this is function execution
+
+function addTwoNumbers(num1, num2){
+    console.log(num1 + num2)
+}
+
+addTwoNumbers(5,6)
+// funtion with parameters to add two numbers 
